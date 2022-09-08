9be92e7a36a8e8f9837051f31e72a91c4cfc9b9c
Belchior <belchiormacedodefranca@gmail.com>
2022-09-08 15:00:30 -0300

    Seller entity class

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import model.entities.Department;
+import model.entities.Seller;
+import java.util.Date;
 
-import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");
+        Seller seller = new Seller(21,"Bob","Bob@gmail.com",new Date(),2500.35,obj);
         System.out.println(obj);
+        System.out.println(seller);
     }
 }
