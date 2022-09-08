cff83f81f407256bb497753dc00f11e0285d231b
Belchior <belchiormacedodefranca@gmail.com>
2022-09-08 17:16:08 -0300

    implementando findByid

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,21 @@
 package model.dao.impl;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
+import java.sql.*;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
+
+    private Connection conn;
+
+   public SellerDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
 
 
     @Override
@@ -25,8 +35,43 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findbyId(Integer id) {
-        return null;
-    }
+        PreparedStatement st= null;
+        ResultSet rs = null;
+        try{
+            st = conn.prepareStatement(
+
+           "SELECT seller.*,department.Name as DepName"
+            + "FROM seller INNER JOIN department"
+            + "ON seller.DepartmentId = department.Id"
+            + "WHERE seller.Id = ?");
+
+            st.setInt(1,id);
+            rs = st.executeQuery();
+            if(rs.next()){
+                Department dep  = new Department();
+                dep.setId(rs.getInt("Department"));
+                dep.setName(rs.getString("DepName"));
+                Seller obj = new Seller();
+                obj.setId(rs.getInt("Id"));]
+                obj.setName((rs.getString("Name")));
+                obj.setEmail(rs.getString("Email"));
+                obj.setBaseSalary(rs.getDouble("Base Salary"));
+                obj.setBirthDate(rs.getDate("Birth Date"));
+                obj.setDepartment(dep);
+                return obj;
+            }
+            return null;
+
+        }
+        catch(SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally{
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+   }
+
 
     @Override
     public List<Seller> findAll() {
